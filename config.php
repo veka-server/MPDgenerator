49627b640c5751cc954e820df7502b00154552bb
Veka <veka61@laposte.net>
2015-01-09 13:58:04 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+	
+	// config BDD 
+	define("TYPEBASE","mysql");
+	define("USER","root"); /* attention a la case, sous oracle tout majuscule */
+	define("PASS","");
+	define("HOST","localhost");
+	define("DATABASE","testveka"); /* ONLY USE BY MYSQL */
+	define("SID","dl2");/* ONLY USE BY ORACLE */
+
+	define("RANDOM",true); /* ONLY USE BY MYSQL */
